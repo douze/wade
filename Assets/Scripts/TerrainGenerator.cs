11a89e625d9f80df48ee19948e089ae4a7c94ef9
douze <douze@users.noreply.github.com>
2022-01-26 13:19:47 +0100

    Arrange tiles by mesh and use them for random terrain generation

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+
+/// <summary>Generate a terrain using WFC.</summary>
 
 public class TerrainGenerator : MonoBehaviour
 {
@@ -8,6 +11,50 @@ public class TerrainGenerator : MonoBehaviour
     public int height;
     public GameObject tiles;
     public GameObject output;
+
+    /// <summary>Remove all GameObject instances from the ouput node.</summary>
+    public void CleanOuput()
+    {
+        while (output.transform.childCount > 0)
+        {
+            DestroyImmediate(output.transform.GetChild(0).gameObject);
+        }
+    }
+
+
+    /// <summary> Flatten the input tiles (arranged by mehs) to a single list of tiles.</summary>
+    private List<GameObject> Flatten(GameObject root)
+    {
+        List<GameObject> flattenTiles = new List<GameObject>();
+        foreach (Transform child in root.transform)
+        {
+            flattenTiles.Add(child.gameObject);
+            flattenTiles.AddRange(Flatten(child.gameObject));
+        }
+        return flattenTiles;
+    }
+
+    /// <summary> Generate a square grid using random tiles.</summary>
+    /// <remarks> For testing purpose only. </remarks>
+    public void GenerateRandomGrid()
+    {
+        List<GameObject> flattenTiles = Flatten(tiles);
+        int tileSize = 2;
+        
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                GameObject tile = GameObject.Instantiate(flattenTiles[Random.Range(0, flattenTiles.Count)], output.transform);
+                tile.transform.position = new Vector3(i * tileSize, 0, j * tileSize);
+
+                while (tile.transform.childCount > 0)
+                {
+                    DestroyImmediate(tile.transform.GetChild(0).gameObject);
+                }
+            }
+        }
+    }
 
 }
 
@@ -21,25 +68,8 @@ public class TerrainGenerator_Inspector : Editor
         TerrainGenerator terrainGenerator = (TerrainGenerator)target;
         if (GUILayout.Button("Generate"))
         {
-            // Clean
-            while (terrainGenerator.output.transform.childCount > 0)
-            {
-                DestroyImmediate(terrainGenerator.output.transform.GetChild(0).gameObject);
-            }
-
-            // Generate
-            int count = terrainGenerator.tiles.transform.childCount;
-            int tileSize = 2;
-
-            for (int j = 0; j < terrainGenerator.height; j++)
-            {
-                for (int i = 0; i < terrainGenerator.width; i++)
-                {
-                    GameObject tile = GameObject.Instantiate(terrainGenerator.tiles.transform.GetChild(Random.Range(0, count)).gameObject, terrainGenerator.output.transform);
-                    tile.transform.position = new Vector3(i*tileSize, 0, j*tileSize);
-                }
-            }
-
+            terrainGenerator.CleanOuput();
+            terrainGenerator.GenerateRandomGrid();
         }
     }
 }
